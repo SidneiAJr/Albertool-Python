9a6ed11c42907c5d71a6b3cd5b323832fc4038c8
Sid Jr <sidalbertok@gmail.com>
2026-06-21 14:32:09 -0300

    Update Tkinter.py

--- Esqueletos/Tkinter.py
+++ Esqueletos/Tkinter.py
@@ -1,1 +1,17 @@
-def tela():
+import tkinter as tk
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("App")
+        self.root.geometry("800x600")
+        self._criar_widgets()
+
+    def _criar_widgets(self):
+        # seus widgets aqui
+        pass
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
